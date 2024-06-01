ee7e2d44b40a94a4ec5c6e8b212120728caae642
AthiraGireesh16 <athiragireesh16@gmail.com>
2024-06-01 09:57:59 +0530

    added payment helper the variables

--- helpers/paymentHelper.js
+++ helpers/paymentHelper.js
@@ -1,5 +1,5 @@
 const Razorpay = require('razorpay');
-const{ RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET} = process.env;
+
 const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET
